50d4c2f7975b5469b77632d1cffb9b0e0a7c88ef
ducnguyen1959 <ducncm99@gmail.com>
2020-11-17 21:42:02 +0700

    up

--- SS1/troll.js
+++ SS1/troll.js
@@ -1,12 +1,12 @@
 let $no = document.getElementById ('no')
 let $yes = document.getElementById('yes')
-
+let $answer = document.getElementById('answer')
 //cách 1: thêm listener 
-// $no.addEventListener('mouseover',function(){
-//     alert ('không')
-// })  
+$no.addEventListener('mouseover',function(){
+    $answer.classList.toggle('reverse')
+})  
 
 //cách 2: ghi đè listener
-$no.onmouseover =function (){
-    alert ('không')
-}
+// $no.onmouseover =function (){
+//     alert ('không')
+// }
