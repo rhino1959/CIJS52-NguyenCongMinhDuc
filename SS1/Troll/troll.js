cdc7ebc04c2370713198d3795873a2a3cf0bf0a0
ducnguyen1959 <ducncm99@gmail.com>
2020-11-17 21:52:30 +0700

    up

--- SS1/Troll/troll.js
+++ SS1/Troll/troll.js
@@ -1,9 +1,17 @@
 let $no = document.getElementById ('no')
 let $yes = document.getElementById('yes')
 let $answer = document.getElementById('answer')
+let count = 0; 
 //cách 1: thêm listener 
 $no.addEventListener('mouseover',function(){
-    $answer.classList.toggle('reverse')
+    count++;
+    if (count<5) {
+        $answer.classList.toggle('reverse') // bật tắt reverse tại nơi có class trống
+    }
+    else {
+        $answer.style.display = "none";
+        alert('Thank you')
+    }
 })  
 
 //cách 2: ghi đè listener
