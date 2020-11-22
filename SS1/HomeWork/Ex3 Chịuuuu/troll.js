5c70d551ff3acb43dba911fcb33a5f8f52ff222d
ducnguyen1959 <ducncm99@gmail.com>
2020-11-23 01:33:25 +0700

    up

--- SS1/HomeWork/Ex3 Chịuuuu/troll.js
+++ SS1/HomeWork/Ex3 Chịuuuu/troll.js
@@ -2,19 +2,14 @@ let $no = document.getElementById ('no')
 let $yes = document.getElementById('yes')
 let $answer = document.getElementById('answer')
 let count = 0; 
-//cách 1: thêm listener 
 $no.addEventListener('mouseover',function(){
     count++;
     if (count<5) {
-        $answer.classList.toggle('reverse') // bật tắt reverse tại nơi có class trống
+        $answer.classList.toggle('reverse')
     }
     else {
-        $answer.style.display = "none";
+        $answer.style.display = "random";
         alert('I love you too')
     }
 })  
 
-//cách 2: ghi đè listener
-// $no.onmouseover =function (){
-//     alert ('không')
-// }
